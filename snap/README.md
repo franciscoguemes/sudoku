0f9e1ea603f2925a449b76786182a3cb39be3e25
franciscoguemes <francisco.jose.guemes.sevilla@gmail.com>
2026-02-21 20:08:30 +0200

    Modify files for creating the snap package locally

--- snap/README.md
+++
@@ -1,3 +0,0 @@
-__NOTE__: Review all contents in this directory after refactoring project setup and CI/CD strategy
-
-The files in this directory are placed here to generate a snap file locally.
--- snap/local/sudoku-launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Use the JRE bundled inside the snap
-JAVA="$SNAP/jre/bin/java"
-
-exec "$JAVA" \
-  --module-path "$SNAP/javafx/lib" \
-  --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.web \
-  -jar "$SNAP/sudoku.jar" "$@"
